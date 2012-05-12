8462cb843cfc0a653c118ca476f3ade988bd3d95
Juho Gävert <jgavert@cs.helsinki.fi>
2012-05-12 18:36:50 +0300

    started work on getting cube to rotate...

--- GLOpenTKDemo/GLOpenTKDemo/Demo.cs
+++ GLOpenTKDemo/GLOpenTKDemo/Demo.cs
@@ -15,7 +15,7 @@ namespace GLOpenTKDemo
 {
     class Demo : GameWindow
     {
-        private int VaoId, VboId, ColorBufferId, VertexShaderId, FragmentShaderId, ProgramId;
+        private int VaoId, VboId, ColorBufferId;
         private Shaders shader;
         /// <summary>Creates a 800x600 window with the specified title.</summary>
         public Demo()
@@ -37,87 +37,98 @@ namespace GLOpenTKDemo
             GL.ClearColor(0.33f, 0.2f, 0.5f, 0.25f);
             GL.Enable(EnableCap.DepthTest);
             shader.Initialize();
-            /*
-            VertexShaderId = GL.CreateShader(ShaderType.VertexShader);
-            //String vertex = Properties.Resources.simplevertex;
-            //System.Console.WriteLine(vertex);
-            //GL.ShaderSource(VertexShaderId, Properties.Resources.simplevertex);
-            GL.CompileShader(VertexShaderId);
-
-            FragmentShaderId = GL.CreateShader(ShaderType.FragmentShader);
-            //GL.ShaderSource(FragmentShaderId, Properties.Resources.simplefragment);
-            GL.CompileShader(FragmentShaderId);
-
-            ProgramId = GL.CreateProgram();
-            GL.AttachShader(ProgramId, VertexShaderId);
-            GL.AttachShader(ProgramId, FragmentShaderId);
-
-            GL.LinkProgram(ProgramId);
-            GL.UseProgram(ProgramId);
-            */
             ErrorCheckValue = GL.GetError();
             if (ErrorCheckValue != ErrorCode.NoError)
                 Trace.WriteLine("Error at Creating Shaders: " + ErrorCheckValue);
 
             // Creating a VBO object now, so that ugly GL.Begin() code can fly to space.
-            // Besides this needs to be done only once, those loops slowing my opengl lol. Unlimited FPS BOOST!
-            // This is still awesome to create torus algorithmically :O! We will be doing more of this.
-            // Koura, where is that genNiceBoat() algorithm .3
-            // Unlimited commenting works.
             float[] Vertices = {
-        -0.4f, -0.4f, 0.0f, 1.0f,
-         0.0f, 0.4f, 0.0f, 1.0f,
-         0.4f, -0.4f, 0.0f, 1.0f
+        -0.4f, -0.4f, 0.4f, 1.0f,
+        -0.4f,  0.4f, 0.4f, 1.0f,
+         0.4f,  0.4f, 0.4f, 1.0f,
+         0.4f,  0.4f, 0.4f, 1.0f,
+         0.4f, -0.4f, 0.4f, 1.0f,
+        -0.4f, -0.4f, 0.4f, 1.0f,
+
+        -0.4f, -0.4f, -0.4f, 1.0f,
+        -0.4f,  0.4f, -0.4f, 1.0f,
+         0.4f,  0.4f, -0.4f, 1.0f,
+         0.4f,  0.4f, -0.4f, 1.0f,
+         0.4f, -0.4f, -0.4f, 1.0f,
+        -0.4f, -0.4f, -0.4f, 1.0f,
+
+         -0.4f,-0.4f, -0.4f, 1.0f,
+         -0.4f,-0.4f,  0.4f, 1.0f,
+         -0.4f, 0.4f,  0.4f, 1.0f,
+         -0.4f, 0.4f,  0.4f, 1.0f,
+         -0.4f, 0.4f, -0.4f, 1.0f,
+         -0.4f,-0.4f, -0.4f, 1.0f,
+
+         0.4f,-0.4f, -0.4f, 1.0f,
+         0.4f,-0.4f,  0.4f, 1.0f,
+         0.4f, 0.4f,  0.4f, 1.0f,
+         0.4f, 0.4f,  0.4f, 1.0f,
+         0.4f, 0.4f, -0.4f, 1.0f,
+         0.4f,-0.4f, -0.4f, 1.0f,
+
+        -0.4f, 0.4f,-0.4f, 1.0f,
+        -0.4f, 0.4f, 0.4f, 1.0f,
+         0.4f, 0.4f, 0.4f, 1.0f,
+         0.4f, 0.4f, 0.4f, 1.0f,
+         0.4f, 0.4f,-0.4f, 1.0f,
+        -0.4f, 0.4f,-0.4f, 1.0f,
+
+        -0.4f, -0.4f,-0.4f, 1.0f,
+        -0.4f, -0.4f, 0.4f, 1.0f,
+         0.4f, -0.4f, 0.4f, 1.0f,
+         0.4f, -0.4f, 0.4f, 1.0f,
+         0.4f, -0.4f,-0.4f, 1.0f,
+        -0.4f, -0.4f,-0.4f, 1.0f
     };
             float[] Colors = {
         1.0f, 0.0f, 0.0f, 1.0f,
+        1.0f, 0.0f, 0.0f, 1.0f,
+        1.0f, 0.0f, 0.0f, 1.0f,
+        1.0f, 0.0f, 0.0f, 1.0f,
+        1.0f, 0.0f, 0.0f, 1.0f,
+        1.0f, 0.0f, 0.0f, 1.0f,
+
         0.0f, 1.0f, 0.2f, 1.0f,
+        0.0f, 1.0f, 0.2f, 1.0f,
+        0.0f, 1.0f, 0.2f, 1.0f,
+        0.0f, 1.0f, 0.2f, 1.0f,
+        0.0f, 1.0f, 0.2f, 1.0f,
+        0.0f, 1.0f, 0.2f, 1.0f,  
+
+        0.2f, 0.0f, 1.0f, 1.0f,
+        0.2f, 0.0f, 1.0f, 1.0f,
+        0.2f, 0.0f, 1.0f, 1.0f,
+        0.2f, 0.0f, 1.0f, 1.0f,
+        0.2f, 0.0f, 1.0f, 1.0f,
+        0.2f, 0.0f, 1.0f, 1.0f,
+
+        1.0f, 0.0f, 0.0f, 1.0f,
+        1.0f, 0.0f, 0.0f, 1.0f,
+        1.0f, 0.0f, 0.0f, 1.0f,
+        1.0f, 0.0f, 0.0f, 1.0f,
+        1.0f, 0.0f, 0.0f, 1.0f,
+        1.0f, 0.0f, 0.0f, 1.0f,
+
+        0.0f, 1.0f, 0.2f, 1.0f,
+        0.0f, 1.0f, 0.2f, 1.0f,
+        0.0f, 1.0f, 0.2f, 1.0f,
+        0.0f, 1.0f, 0.2f, 1.0f,
+        0.0f, 1.0f, 0.2f, 1.0f,
+        0.0f, 1.0f, 0.2f, 1.0f,  
+
+        0.2f, 0.0f, 1.0f, 1.0f,
+        0.2f, 0.0f, 1.0f, 1.0f,
+        0.2f, 0.0f, 1.0f, 1.0f,
+        0.2f, 0.0f, 1.0f, 1.0f,
+        0.2f, 0.0f, 1.0f, 1.0f,
         0.2f, 0.0f, 1.0f, 1.0f
     };
-            /*
-            int lol = 0;
-            int numc = 50;
-            int numt = 50;
-
-            int i, j, k;
-            double s, t, x, y, z, twopi;
-            float[] Vertices = new float[numc * (numt+1) * 4 * 2];
-            float[] Colors = new float[numc * (numt+1) * 4 * 2];
-
-            twopi = 2 * Math.PI;
-            for (i = 0; i < numc; i++)
-            {
-                //GL.Begin(BeginMode.QuadStrip);
-                for (j = 0; j <= numt; j++)
-                {
-                    for (k = 1; k >= 0; k--)
-                    {
-                        ++lol;
-                        s = (i + k) % numc + 0.5;
-                        t = j % numt;
-
-                        x = (1 + .1 * Math.Cos(s * twopi / numc)) * Math.Cos(t * twopi / numt);
-                        y = (1 + .1 * Math.Cos(s * twopi / numc)) * Math.Sin(t * twopi / numt);
-                        z = .1 * Math.Sin(s * twopi / numc) + 4;
-
-                        Colors[((i + 1) * (j + 1) * 4 * (k + 1)) - 4] = 0.2f;
-                        Colors[((i + 1) * (j + 1) * 4 * (k + 1) + 1) - 4] = 0.9f;
-                        Colors[((i + 1) * (j + 1) * 4 * (k + 1) + 2) - 4] = 1.0f;
-                        Colors[((i + 1) * (j + 1) * 4 * (k + 1) + 3) - 4] = 1.0f;
-                        //GL.Color3(0.2f, 0.9f, 1.0f);
-                        Vertices[((i + 1) * (j + 1) * 4 * (k + 1)) - 4] = (float)x;
-                        Vertices[((i + 1) * (j + 1) * 4 * (k + 1) + 1) - 4] = (float)y;
-                        Vertices[((i + 1) * (j + 1) * 4 * (k + 1) + 2) - 4] = (float)z;
-                        Vertices[((i + 1) * (j + 1) * 4 * (k + 1) + 3) - 4] = 1.0f;
-                        //GL.Vertex3(x, y, z);
-                    }
-                }
-                //GL.End();
-            }*/
-            //System.Console.WriteLine("Torus had " + (lol*4) + " vertices. Expected " + (200*201*4*2) + " vertices.");
-            //System.Console.WriteLine("Initialized these: Vertices.length " + Vertices.Length + ".");
             
-
             GL.GenVertexArrays(1, out VaoId);
             GL.BindVertexArray(VaoId);
 
@@ -185,7 +196,7 @@ namespace GLOpenTKDemo
             Matrix4 modelview = Matrix4.LookAt(Vector3.Zero, Vector3.UnitZ, Vector3.UnitY);
             GL.MatrixMode(MatrixMode.Modelview);
             GL.LoadMatrix(ref modelview);
-            GL.DrawArrays(BeginMode.Triangles, 0, 3*4);
+            GL.DrawArrays(BeginMode.Triangles, 0, 6*6*4);
             /*
             GL.Begin(BeginMode.Triangles);
 
