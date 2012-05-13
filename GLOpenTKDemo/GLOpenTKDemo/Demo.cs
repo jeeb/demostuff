e0022dc5a339ac856b30c8fe58869315c49c26ed
Juho Gävert <jgavert@cs.helsinki.fi>
2012-05-13 07:04:25 +0300

    Started work on dividing to classes

--- GLOpenTKDemo/GLOpenTKDemo/Demo.cs
+++ GLOpenTKDemo/GLOpenTKDemo/Demo.cs
@@ -15,22 +15,16 @@ namespace GLOpenTKDemo
 {
     class Demo : GameWindow
     {
-        int modelLocation;
-        int viewLocation;
-        int projectionLocation;
-        int timeLocation;
-        int resoLocation;
-        Vector2 resolution; 
-        Matrix4 ModelMatrix;
-        Matrix4 ViewMatrix;
-        Matrix4 ProjectionMatrix;
-        private int VaoId, VboId, ColorBufferId;
-        private Shaders shader;
+        int fps;
+        long time;
+        Graphics graphics;
         /// <summary>Creates a 800x600 window with the specified title.</summary>
         public Demo()
             : base(1280, 720, new GraphicsMode(32, 24, 8, 0), "The Demo Effect", GameWindowFlags.Default, DisplayDevice.Default, 3, 3, GraphicsContextFlags.Debug)
         {
-            VSync = VSyncMode.On;
+            VSync = VSyncMode.Off;
+            fps = 0;
+            time = System.DateTime.Now.Millisecond + (System.DateTime.Now.Second * 1000) + (System.DateTime.Now.Minute * 60000);
         }
 
         /// <summary>Load resources here.</summary>
@@ -38,87 +32,10 @@ namespace GLOpenTKDemo
         protected override void OnLoad(EventArgs e)
         {
             base.OnLoad(e);
+            graphics = new Graphics();
+            graphics.Initialize();
             // Compose a string that consists of three lines.
-            shader = new Shaders();
 
-
-            ErrorCode ErrorCheckValue = GL.GetError();
-            GL.ClearColor(0.33f, 0.2f, 0.5f, 0.25f);
-            GL.Enable(EnableCap.DepthTest);
-            ModelMatrix = Matrix4.Identity;
-            ViewMatrix = Matrix4.Identity;
-            ProjectionMatrix = Matrix4.Identity;
-            GL.Enable(EnableCap.DepthTest);
-            GL.DepthFunc(DepthFunction.Less);
-            GL.Enable(EnableCap.CullFace);
-            GL.CullFace(CullFaceMode.Back);
-            GL.FrontFace(FrontFaceDirection.Ccw);
-
-            shader.Initialize();
-            ErrorCheckValue = GL.GetError();
-            if (ErrorCheckValue != ErrorCode.NoError)
-                Trace.WriteLine("Error at Creating Shaders: " + ErrorCheckValue);
-
-            // Creating a VBO object now, so that ugly GL.Begin() code can fly to space.
-            float[] Vertices = {
-           -.5f, -.5f,  .5f, 1 , 
-           -.5f,  .5f,  .5f, 1,
-            .5f,  .5f,  .5f, 1 , 
-            .5f, -.5f,  .5f, 1 , 
-           -.5f, -.5f, -.5f, 1 , 
-           -.5f,  .5f, -.5f, 1 ,
-            .5f,  .5f, -.5f, 1 , 
-            .5f, -.5f, -.5f, 1 , 
-
-
-          };
-
-        float[] Colors = {
-         0, 0, 1, 1 ,
-         1, 0, 0, 1,
-         0, 1, 0, 1,
-         1, 1, 0, 1 ,
-         1, 1, 1, 1,
-         1, 0, 0, 1,
-         1, 0, 1, 1,
-         0, 0, 1, 1
-    };
-
-            uint[] indices = {
-            0,2,1, 0,3,2,
-            4,3,0, 4,7,3,
-            4,1,5, 4,0,1,
-            3,6,2, 3,7,6,
-            1,6,5, 1,2,6,
-            7,5,6, 7,4,5
-        };
-            
-            modelLocation = GL.GetUniformLocation(shader.ProgramIds[0], "ModelMatrix");
-            viewLocation = GL.GetUniformLocation(shader.ProgramIds[0], "ViewMatrix");
-            projectionLocation = GL.GetUniformLocation(shader.ProgramIds[0], "ProjectionMatrix");
-            timeLocation = GL.GetUniformLocation(shader.ProgramIds[0], "time");
-            resoLocation = GL.GetUniformLocation(shader.ProgramIds[0], "resolution");
-
-            resolution = new Vector2(1280, 720);
-
-            GL.GenVertexArrays(1, out VaoId);
-            GL.BindVertexArray(VaoId);
-            //derp cubeg
-            GL.GenBuffers(1, out VboId);
-            GL.BindBuffer(BufferTarget.ArrayBuffer, VboId);
-            GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(Vertices.Length * sizeof(float)), Vertices, BufferUsageHint.StaticDraw);
-            GL.VertexAttribPointer(0, 4, VertexAttribPointerType.Float, false, 0, 0);
-            GL.EnableVertexAttribArray(0);
-            //our pretty colours
-            GL.GenBuffers(1, out ColorBufferId);
-            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ColorBufferId);
-            GL.BufferData(BufferTarget.ElementArrayBuffer, (IntPtr)(indices.Length * sizeof(float)), indices, BufferUsageHint.StaticDraw);
-            GL.VertexAttribPointer(1, 4, VertexAttribPointerType.Float, false, 0, 0);
-            GL.EnableVertexAttribArray(1);
-
-            ErrorCheckValue = GL.GetError();
-            if (ErrorCheckValue != ErrorCode.NoError)
-                Trace.WriteLine("Error at Creating VBO: " + ErrorCheckValue);
         }
 
         /// <summary>
@@ -131,13 +48,7 @@ namespace GLOpenTKDemo
         {
             base.OnResize(e);
 
-            GL.Viewport(ClientRectangle.X, ClientRectangle.Y, ClientRectangle.Width, ClientRectangle.Height);
-
-            Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView((float)Math.PI / 4, Width/(float)Height, 1.0f, 90.0f);
-            GL.MatrixMode(MatrixMode.Projection);
-            GL.LoadMatrix(ref projection);
-            ProjectionMatrix = projection;
-
+            graphics.onResize(ClientRectangle.X, ClientRectangle.Y, ClientRectangle.Width, ClientRectangle.Height, Width, Height);
         }
 
         /// <summary>
@@ -150,45 +61,61 @@ namespace GLOpenTKDemo
 
             if (Keyboard[Key.Escape])
                 Exit();
-            if (Keyboard[Key.Left])
-                Exit();
             if (Keyboard[Key.F5])
-                shader.Update();
+                graphics.shaderUpdate();
+            if (Keyboard[Key.F])
+                WindowState = WindowState.Fullscreen;
+            if (Keyboard[Key.W])
+                WindowState = WindowState.Normal;
             if (Keyboard[Key.H])//left
             {
-                Matrix4 heippa = ModelMatrix;
-                Matrix4 heippa2 = Matrix4.CreateRotationY(0.2f);
-                Matrix4.Mult(ref heippa, ref heippa2, out ModelMatrix);
+                graphics.rotateObjectByY(0.2f);
             }
             if (Keyboard[Key.L]) //right
             {
-                Matrix4 heippa = ModelMatrix;
-                Matrix4 heippa2 = Matrix4.CreateRotationY(-0.2f);
-                Matrix4.Mult(ref heippa, ref heippa2, out ModelMatrix);
+                graphics.rotateObjectByY(-0.2f);
             }
             if (Keyboard[Key.J]) //up
             {
-                Matrix4 heippa = ModelMatrix;
-                Matrix4 heippa2 = Matrix4.CreateRotationX(0.2f);
-                Matrix4.Mult(ref heippa, ref heippa2, out ModelMatrix);
+                graphics.rotateObjectByX(0.2f);
             }
             if (Keyboard[Key.K]) //down
             {
-                Matrix4 heippa = ModelMatrix;
-                Matrix4 heippa2 = Matrix4.CreateRotationX(-0.2f);
-                Matrix4.Mult(ref heippa, ref heippa2, out ModelMatrix);
+                graphics.rotateObjectByX(-0.2f);
             }
-            if (Keyboard[Key.U]) //down
+            if (Keyboard[Key.U])
             {
-                Matrix4 heippa = ModelMatrix;
-                Matrix4 heippa2 = Matrix4.CreateRotationZ(0.2f);
-                Matrix4.Mult(ref heippa, ref heippa2, out ModelMatrix);
+                graphics.rotateObjectByZ(0.2f);
             }
-            if (Keyboard[Key.I]) //down
+            if (Keyboard[Key.I])
             {
-                Matrix4 heippa = ModelMatrix;
-                Matrix4 heippa2 = Matrix4.CreateRotationZ(-0.2f);
-                Matrix4.Mult(ref heippa, ref heippa2, out ModelMatrix);
+                graphics.rotateObjectByZ(-0.2f);
+            }
+
+
+            if (Keyboard[Key.Up])//left
+            {
+                graphics.moveCameraBy(0.0f, 0.2f, 0.0f);
+            }
+            if (Keyboard[Key.Down]) //right
+            {
+                graphics.moveCameraBy(0.0f, -0.2f, 0.0f);
+            }
+            if (Keyboard[Key.Right]) //up
+            {
+                graphics.moveCameraBy(0.2f, 0.0f, 0.0f);
+            }
+            if (Keyboard[Key.Left]) //down
+            {
+                graphics.moveCameraBy(-0.2f, 0.0f, 0.0f);
+            }
+            if (Keyboard[Key.N])
+            {
+                graphics.moveCameraBy(0.0f, 0.0f, 0.2f);
+            }
+            if (Keyboard[Key.M])
+            {
+                graphics.moveCameraBy(0.0f, 0.0f, -0.2f);
             }
         }
 
@@ -198,38 +125,18 @@ namespace GLOpenTKDemo
         /// <param name="e">Contains timing information.</param>
         protected override void OnRenderFrame(FrameEventArgs e)
         {
+    
             base.OnRenderFrame(e);
-            GL.UseProgram(shader.ProgramIds[0]);
-
-            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-            Matrix4 modelview = Matrix4.CreateTranslation(new Vector3(0, 0, -2));
-            GL.MatrixMode(MatrixMode.Modelview);
-            GL.LoadMatrix(ref modelview);
-            ViewMatrix = modelview;
-            float time = System.DateTime.Now.Millisecond * 0.01f;
-            GL.Uniform1(timeLocation, 1, ref time);
-            GL.Uniform2(resoLocation, ref resolution);
-            GL.UniformMatrix4(modelLocation, false, ref ModelMatrix);
-            GL.UniformMatrix4(viewLocation, false, ref ViewMatrix);
-            GL.UniformMatrix4(projectionLocation, false, ref ProjectionMatrix);
-
-           
-
-
-            //System.Console.WriteLine(time);
-            GL.DrawElements(BeginMode.Triangles, 36,DrawElementsType.UnsignedInt, 0);
-            /*
-            GL.Begin(BeginMode.Triangles);
-
-            GL.Color3(1.0f, 1.0f, 0.0f); GL.Vertex3(-1.0f, -1.0f, 4.0f);
-            GL.Color3(1.0f, 0.0f, 0.0f); GL.Vertex3(1.0f, -1.0f, 4.0f);
-            GL.Color3(0.2f, 0.9f, 1.0f); GL.Vertex3(0.0f, 1.0f, 4.0f);
-
-            GL.End();
-            */
-
-            GL.UseProgram(0);
+            graphics.Render();
             SwapBuffers();
+            fps++;
+            long now = System.DateTime.Now.Millisecond + (System.DateTime.Now.Second * 1000) + (System.DateTime.Now.Minute * 60000);
+            if (now - time > 1000)
+            {
+                base.Title = "DemoEffect  Fps: " + fps + " frames per second.";
+                fps = 0;
+                time = now;
+            }
         }
 
         /// <summary>
---
+++ GLOpenTKDemo/GLOpenTKDemo/Graphics.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using System.Diagnostics;
+
+namespace GLOpenTKDemo
+{
+    class Graphics
+    {
+        int modelLocation;
+        int viewLocation;
+        int projectionLocation;
+        int timeLocation;
+        int resoLocation;
+        Vector2 resolution;
+        Matrix4 ModelMatrix;
+        Matrix4 ViewMatrix;
+        Matrix4 ProjectionMatrix;
+        private int VaoId, VboId, ColorBufferId;
+        private Shaders shader;
+
+
+        public Graphics(){}
+
+        public void Initialize()
+        {
+            shader = new Shaders();
+
+
+            ErrorCode ErrorCheckValue = GL.GetError();
+            GL.ClearColor(0.33f, 0.2f, 0.5f, 0.25f);
+            GL.Enable(EnableCap.DepthTest);
+            ModelMatrix = Matrix4.Identity;
+            ViewMatrix = Matrix4.Identity;
+            ProjectionMatrix = Matrix4.Identity;
+            GL.Enable(EnableCap.DepthTest);
+            GL.DepthFunc(DepthFunction.Less);
+            GL.Enable(EnableCap.CullFace);
+            GL.CullFace(CullFaceMode.Back);
+            GL.FrontFace(FrontFaceDirection.Ccw);
+
+            shader.Initialize();
+            ErrorCheckValue = GL.GetError();
+            if (ErrorCheckValue != ErrorCode.NoError)
+                Trace.WriteLine("Error at Creating Shaders: " + ErrorCheckValue);
+
+            // Creating a VBO object now, so that ugly GL.Begin() code can fly to space.
+            float[] Vertices = {
+           -.5f, -.5f,  .5f, 1 , 
+           -.5f,  .5f,  .5f, 1,
+            .5f,  .5f,  .5f, 1 , 
+            .5f, -.5f,  .5f, 1 , 
+           -.5f, -.5f, -.5f, 1 , 
+           -.5f,  .5f, -.5f, 1 ,
+            .5f,  .5f, -.5f, 1 , 
+            .5f, -.5f, -.5f, 1 , 
+
+
+          };
+
+            float[] Colors = {
+         0, 0, 1, 1 ,
+         1, 0, 0, 1,
+         0, 1, 0, 1,
+         1, 1, 0, 1 ,
+         1, 1, 1, 1,
+         1, 0, 0, 1,
+         1, 0, 1, 1,
+         0, 0, 1, 1
+    };
+
+            uint[] indices = {
+            0,2,1, 0,3,2,
+            4,3,0, 4,7,3,
+            4,1,5, 4,0,1,
+            3,6,2, 3,7,6,
+            1,6,5, 1,2,6,
+            7,5,6, 7,4,5
+        };
+
+            modelLocation = GL.GetUniformLocation(shader.ProgramIds[0], "ModelMatrix");
+            viewLocation = GL.GetUniformLocation(shader.ProgramIds[0], "ViewMatrix");
+            projectionLocation = GL.GetUniformLocation(shader.ProgramIds[0], "ProjectionMatrix");
+            timeLocation = GL.GetUniformLocation(shader.ProgramIds[0], "time");
+            resoLocation = GL.GetUniformLocation(shader.ProgramIds[0], "resolution");
+
+            resolution = new Vector2(1280, 720);
+
+            GL.GenVertexArrays(1, out VaoId);
+            GL.BindVertexArray(VaoId);
+            //derp cubeg
+            GL.GenBuffers(1, out VboId);
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VboId);
+            GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(Vertices.Length * sizeof(float)), Vertices, BufferUsageHint.StaticDraw);
+            GL.VertexAttribPointer(0, 4, VertexAttribPointerType.Float, false, 0, 0);
+            GL.EnableVertexAttribArray(0);
+            //our pretty colours
+            GL.GenBuffers(1, out ColorBufferId);
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ColorBufferId);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, (IntPtr)(indices.Length * sizeof(float)), indices, BufferUsageHint.StaticDraw);
+            GL.VertexAttribPointer(1, 4, VertexAttribPointerType.Float, false, 0, 0);
+            GL.EnableVertexAttribArray(1);
+
+            ErrorCheckValue = GL.GetError();
+            if (ErrorCheckValue != ErrorCode.NoError)
+                Trace.WriteLine("Error at Creating VBO: " + ErrorCheckValue);
+
+
+            Matrix4 modelview = Matrix4.CreateTranslation(new Vector3(0, 0, -2));
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref modelview);
+            ViewMatrix = modelview;
+        }
+
+
+        public void onResize(int a, int b, int c, int d, int x, int y)
+        {
+            GL.Viewport(a, b, c, d);
+
+            Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView((float)Math.PI / 4, x / (float)y, 1.0f, 90.0f);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref projection);
+            ProjectionMatrix = projection;
+        }
+
+        public void Render()
+        {
+            GL.UseProgram(shader.ProgramIds[0]);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            float time = System.DateTime.Now.Millisecond * 0.01f;
+            GL.Uniform1(timeLocation, 1, ref time);
+            GL.Uniform2(resoLocation, ref resolution);
+            GL.UniformMatrix4(modelLocation, false, ref ModelMatrix);
+            GL.UniformMatrix4(viewLocation, false, ref ViewMatrix);
+            GL.UniformMatrix4(projectionLocation, false, ref ProjectionMatrix);
+
+
+
+
+            //System.Console.WriteLine(time);
+            GL.DrawElements(BeginMode.Triangles, 36, DrawElementsType.UnsignedInt, 0);
+            /*
+            GL.Begin(BeginMode.Triangles);
+
+            GL.Color3(1.0f, 1.0f, 0.0f); GL.Vertex3(-1.0f, -1.0f, 4.0f);
+            GL.Color3(1.0f, 0.0f, 0.0f); GL.Vertex3(1.0f, -1.0f, 4.0f);
+            GL.Color3(0.2f, 0.9f, 1.0f); GL.Vertex3(0.0f, 1.0f, 4.0f);
+
+            GL.End();
+            */
+
+            GL.UseProgram(0);
+        }
+
+        public void rotateObjectByX(float x)
+        {
+            Matrix4 heippa = ModelMatrix;
+            Matrix4 heippa2 = Matrix4.CreateRotationX(x);
+            Matrix4.Mult(ref heippa, ref heippa2, out ModelMatrix);
+        }
+
+        public void rotateObjectByY(float y)
+        {
+            Matrix4 heippa = ModelMatrix;
+            Matrix4 heippa2 = Matrix4.CreateRotationY(y);
+            Matrix4.Mult(ref heippa, ref heippa2, out ModelMatrix);
+        }
+
+        public void rotateObjectByZ(float z)
+        {
+            Matrix4 heippa = ModelMatrix;
+            Matrix4 heippa2 = Matrix4.CreateRotationZ(z);
+            Matrix4.Mult(ref heippa, ref heippa2, out ModelMatrix);
+        }
+
+        public void translateObject(float x, float y, float z)
+        {
+            Matrix4 heippa = ModelMatrix;
+            Matrix4 heippa2 = Matrix4.CreateTranslation(new Vector3(x, y, z));
+            Matrix4.Mult(ref heippa, ref heippa2, out ModelMatrix);
+        }
+
+        public void shaderUpdate()
+        {
+            shader.Update();
+        }
+
+        public void moveCameraBy(float x, float y, float z)
+        {
+            Matrix4 heh = Matrix4.CreateTranslation(new Vector3(x, y, z));
+            Matrix4 asd = ViewMatrix;
+            ViewMatrix = Matrix4.Mult(asd, heh);
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadMatrix(ref ViewMatrix);
+        }
+    }
+}
